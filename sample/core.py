changed the sign of beta back to the original after checking that is how it should be.The wrong sign of beta only works better for test_elas and nont for _uimp, shoing that there is still something wrong with the free boundaries

sample/core.py
@@ -194,7 +194,7 @@ class ElasticProblem:
             convergence_hist.append(res_max_convergence)
 
             #Calculate new search direction
-            beta = ((np.dot(resx.ravel(),a_d_x.ravel())
+            beta = -((np.dot(resx.ravel(),a_d_x.ravel())
                      + np.dot(resy.ravel(),a_d_y.ravel()))
                     / d_a_d)
             dx = resx + beta*dx

tests/test_elas_uimp.py
@@ -33,7 +33,7 @@ uy_imp[bulk] = np.nan
 px_bound = np.zeros(solid.shape)
 py_bound = np.zeros(solid.shape)
 fx_imp = np.zeros(solid.shape)
-fx_imp[int((nx-1)/2),int((ny-1)/2)] = 20
+#fx_imp[int((nx-1)/2),int((ny-1)/2)] = 20
 
 
 