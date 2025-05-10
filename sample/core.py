9505b2c32134ec0d17c86e54471583af80a77d0c
romain-bugnicourt <rbugnicourt@free.FR>
2025-05-10 22:35:43 +0200

    Frontier adjustment debug // master. SHould be checked. Tests NOK (corners not great, no convergence on pressure vessel)

--- sample/core.py
+++ sample/core.py
@@ -298,8 +298,9 @@ class ElasticProblem:
 
         # Adjust defs on frontier
         coef = - self.elas_lambda / (self.elas_lambda + 2 * self.elas_mu)
-        exx[self.x_frontier_def] = coef * eyy[self.x_frontier_def]
-        eyy[self.y_frontier_def] = coef * exx[self.y_frontier_def]
+        temp=exx[self.y_frontier_def]
+        exx[self.x_frontier_def] += coef * eyy[self.x_frontier_def]
+        eyy[self.y_frontier_def] += coef * temp
 
 
         #Average + mod to have def on edges _x perpendicular to x, and _y perpendicular to y
