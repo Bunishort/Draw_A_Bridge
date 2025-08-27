3c462ded2c1c571492499eea154e1e0701b67b6e
romain-bugnicourt <rbugnicourt@free.FR>
2025-08-27 22:28:11 +0200

    Parameters changes for comparison with master

--- tests/test_pressure_vessel.py
+++ tests/test_pressure_vessel.py
@@ -8,13 +8,13 @@ from scipy.interpolate import interpn
 import timeit
 start = timeit.default_timer()
 
-nx=64
-ny=64
+nx=2*64
+ny=2*64
 L = 25
 lm= L/nx
 max_res = 1e-6
 
-ri = 3#internal radius
+ri = 5#internal radius
 ro = 10#external radius
 pi = 0.1
 
