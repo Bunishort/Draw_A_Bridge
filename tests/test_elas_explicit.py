1f762fac0073e3200c93280fe4e23b754a1f58f6
romain-bugnicourt <rbugnicourt@free.FR>
2025-12-01 21:50:28 +0100

    new explicit test case

---
+++ tests/test_elas_explicit.py
@@ -0,0 +1,65 @@
+from context import sample
+import numpy as np
+import matplotlib.pyplot as plt
+
+k = 3
+nx=k*7
+ny=k*9
+
+lx = k*5
+ly = k*5
+
+x = np.arange(nx) - (nx-1)/2
+y = np.arange(ny) - (ny-1)/2
+
+max_res = 1e-6
+
+gridy,gridx = np.meshgrid(y,x)
+
+solid = np.zeros([nx,ny],dtype=bool)
+solid[np.bitwise_and(np.abs(gridx)<=lx/2,
+    np.abs(gridy)<=ly/2)] = True
+
+max_iter=1000
+E=1
+nu = 0.4
+px = 0.01
+
+elas_lambda = E*nu /(1+nu)/(1-2*nu)
+elas_mu = E/2/(1+nu)
+lm=1.5
+ux_imp=np.zeros(solid.shape)
+ux_imp[:,:] = np.nan
+ux_imp[gridx < (-lx/2+lm)] =0
+uy_imp=ux_imp
+
+px_bound = np.zeros(solid.shape)
+px_bound[gridx > (lx/2-lm)] = px
+py_bound = np.zeros(solid.shape)
+
+test = sample.core.ElasticProblem(solid,elas_lambda,elas_mu,lm,ux_imp,uy_imp,
+                                  px_bound=px_bound,py_bound=py_bound,max_iter=max_iter,max_res = max_res,
+                                  is_explicit = True)
+
+test.calc_stress_explicit(test.ux, test.uy)
+
+n_iter,resx,resy,res_max_convergence,convergence_hist  = test.cg_loop()
+
+bx, by = test.calc_b()
+a_u_x, a_u_y = test.calc_a_u(test.ux, test.uy)
+resx2 = bx - a_u_x
+resy2 = by - a_u_y
+
+sxx_x,sxy_x,syy_y,sxy_y = test.calc_stress(test.ux, test.uy)
+
+plt.figure()
+plt.plot(np.sum(sxx_x,axis=1))
+
+plt.figure()
+ux_ref = lm*(x - np.min(gridx[solid])) * (1 - nu **2 ) / E * px
+plt.plot(lm*x, ux_ref,'k')
+plt.plot(lm*x, test.ux[:,int(nx/2)])
+plt.show()
+
+
+1+1
