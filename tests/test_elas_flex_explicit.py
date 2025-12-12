4bf8461511a4bbed729092df98ab887022ef7917
romain-bugnicourt <rbugnicourt@free.FR>
2025-12-12 21:55:31 +0100

    Very blurry using openCV gaussian filters and upscaling

--- tests/test_elas_flex_explicit.py
+++ tests/test_elas_flex_explicit.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata, interpn
 from scipy.ndimage import zoom
+import cv2
 
 k = 5
 nx=k*7
@@ -96,7 +97,10 @@ for i in range(0,nstep):
         Z[xi_solid,yi_solid] = test.sxy_y_old[solid]
         #upscaling
         # Zplot = interpn((x,y),Z,(gridxplot,gridyplot),fill_value=np.nan, method="linear", bounds_error=False)
-        Zplot = zoom(Z,zoom=kplot,order=1)
+        # Zplot = zoom(Z,zoom=kplot,order=1)
+
+        Z = cv2.GaussianBlur(Z, (0, 0), sigmaX=1, sigmaY = 1)
+        Zplot = cv2.resize(Z, None, fx=kplot, fy=kplot, interpolation=cv2.INTER_AREA)
         # Zplot = griddata(list(zip(gridxx, gridyy)), test.sxy_y_old[solid],
         #               (gridxplot, gridyplot), method='linear', rescale=False)
         #solution 1 : extrapoler ux partout pour avoir un truc cohérent, continu, bijectif
